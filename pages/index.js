7800f7338e6fc7b591fe632d8fe22179f527bcac
ray8085637-a11y <ray8085637@gmail.com>
2025-08-20 19:05:20 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,13 @@
-export { default } from '../assets/index-DCgDFyKC.js';
+import dynamic from 'next/dynamic';
+
+const WaterTTApp = dynamic(
+  () => import('../assets/index-DCgDFyKC.js'),
+  { 
+    ssr: false,
+    loading: () => <div>Loading...</div>
+  }
+);
+
+export default function Home() {
+  return <WaterTTApp />;
+}
