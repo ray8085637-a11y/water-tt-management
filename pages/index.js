32fd39c5cb38ec760b48e8e776676fae5d05ade3
ray8085637-a11y <ray8085637@gmail.com>
2025-08-20 19:03:32 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,1 @@
-import React from 'react';
-
-// assets/index-DCgDFyKC.js 파일의 내용을 여기에 복사해서 붙여넣으세요
-// 1. GitHub에서 assets/index-DCgDFyKC.js 열기
-// 2. 전체 내용 복사 (Ctrl + A → Ctrl + C)  
-// 3. 이 주석 아래에 붙여넣기 (Ctrl + V)
-
-// 복사한 내용이 여기에 들어가야 합니다
+export { default } from '../assets/index-DCgDFyKC.js';
