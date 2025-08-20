05ecdeb100cf3bd64c78bf1d8d42d656e850abb3
ray8085637-a11y <ray8085637@gmail.com>
2025-08-20 19:00:09 +0900

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  swcMinify: false,
+  experimental: {
+    esmExternals: false
+  }
+}
+
+module.exports = nextConfig
