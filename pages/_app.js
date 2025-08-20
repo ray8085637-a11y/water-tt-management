cf1648fdfdab661ffa3d392867480837129731bb
ray8085637-a11y <ray8085637@gmail.com>
2025-08-20 18:23:12 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css';
-import '../assets/index-D01l6DGN.css';  // 이 줄 추가
+// import '../assets/index-D01L6DGN.css';  // 주석 처리
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
