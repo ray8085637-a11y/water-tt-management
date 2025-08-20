ef15f38ba49ef8dc4e9df3d0f1758ab9c7f1dedf
ray8085637-a11y <ray8085637@gmail.com>
2025-08-20 17:57:23 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,13 @@
-import TaxManagementApp from '../assets/index-DCgDFyKC.js';
+import dynamic from 'next/dynamic';
+
+// 클라이언트에서만 렌더링
+const TaxManagementApp = dynamic(
+  () => import('../assets/index-DCgDFyKC.js'),
+  { 
+    ssr: false,
+    loading: () => <div>로딩 중...</div>
+  }
+);
 
 export default function Home() {
   return <TaxManagementApp />;
